ff64508cbbd16f0f3a4967f5d49c220cf1f72a53
RaufAbd <rufabad.4@gmail.com>
2025-08-04 21:54:36 +0400

    fix: changed  file name settings to be compatible with Windows

--- src/app.js
+++ src/app.js
@@ -34,7 +34,11 @@ const fileStorage = multer.diskStorage({
     cb(null, "images");
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + "-" + file.originalname);
+    const timestamp = new Date()
+      .toISOString()
+      .replace(/:/g, "-")
+      .replace(/\./g, "-");
+    cb(null, timestamp + "-" + file.originalname);
   },
 });
 
